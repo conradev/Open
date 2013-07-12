6384940ac72fcd386d351fb35a90187f59043730
Cykey <cykey@live.com>
2013-07-12 13:40:43 -0400

    Fix memory management.

--- open.m
+++ open.m
@@ -16,12 +16,16 @@ int main(int argc, char **argv, char **envp)
     }
 
     CFStringRef identifier = CFStringCreateWithCString(kCFAllocatorDefault, argv[1], kCFStringEncodingUTF8);
+    assert(identifier != NULL);
+
     ret = SBSLaunchApplicationWithIdentifier(identifier, FALSE);
 
     if (ret != 0) {
         fprintf(stderr, "Couldn't open application: %s. Reason: %i, ", argv[1], ret);
         CFShow(SBSApplicationLaunchingErrorString(ret));
     }
+
+    CFRelease(identifier);
 
     return ret;
 }
